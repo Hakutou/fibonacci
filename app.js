7f67a7f13793e004fdd6aca500697537c4793518
hakutou <s1150143@gmail.com>
2018-09-22 15:25:09 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -4,17 +4,27 @@
 // F1 = 1
 // Fn+2 = fn + fn+1(n >= 0)
 
+const memo = new Map();
+memo.set(0,0);
+memo.set(1,1);
+
 function fib(n){
+    if(memo.has(n)){
+        return memo.get(n);
+    }
     if(n < 0){
         return 0;
     } else if(n === 1){
-        return n;
+        return 1;
     }
-    
-    return fib(n - 2) + fib(n - 1);
+
+    const result = fib(n - 2) + fib(n - 1);
+    memo.set(n, result);
+
+    return result;
 }
 
-const length = 40;
+const length = 100;
 for(let i = 0;i <= length;i++){
     console.log(fib(i));
 }
